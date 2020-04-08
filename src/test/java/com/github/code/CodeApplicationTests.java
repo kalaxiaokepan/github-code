62b1ffb785874cfc522b91791159883e4b4cd4b7
kalaxiaokepan <heyanbo163@163.com>
2020-04-08 21:53:16 +0800

    初始化springboot项目

---
+++ src/test/java/com/github/code/CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.code;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
